5a3ef6e9cff24048dc24f5e5ae86403184b0ec44
sudha-258 <karkisudha21@gmail.com>
2025-11-30 22:11:27 +0545

    Add som libraries

--- simulator.c
+++ simulator.c
@@ -1,20 +1,18 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <stdbool.h>
-#include <pthread.h>
-#include <unistd.h> 
-#include <stdio.h> 
+#include <windows.h>   // For Sleep and threads
+#include <stdio.h>
 #include <string.h>
 
 #define MAX_LINE_LENGTH 20
-#define MAIN_FONT "/usr/share/fonts/TTF/DejaVuSans.ttf"
+#define MAIN_FONT "C:\\Windows\\Fonts\\Arial.ttf"  // Use a Windows font path
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 800
 #define SCALE 1
 #define ROAD_WIDTH 150
 #define LANE_WIDTH 50
 #define ARROW_SIZE 15
-
 
 const char* VEHICLE_FILE = "vehicles.data";
 
@@ -23,23 +21,18 @@ typedef struct{
     int nextLight;
 } SharedData;
 
-
 // Function declarations
 bool initializeSDL(SDL_Window **window, SDL_Renderer **renderer);
 void drawRoadsAndLane(SDL_Renderer *renderer, TTF_Font *font);
 void displayText(SDL_Renderer *renderer, TTF_Font *font, char *text, int x, int y);
 void drawLightForB(SDL_Renderer* renderer, bool isRed);
 void refreshLight(SDL_Renderer *renderer, SharedData* sharedData);
-void* chequeQueue(void* arg);
-void* readAndParseFile(void* arg);
-
-
-void printMessageHelper(const char* message, int count) {
-    for (int i = 0; i < count; i++) printf("%s\n", message);
-}
+DWORD WINAPI chequeQueue(LPVOID arg);
+DWORD WINAPI readAndParseFile(LPVOID arg);
+void drawArrwow(SDL_Renderer* renderer, int x1, int y1, int x2, int y2, int x3, int y3);
+void swap(int *a, int *b);
 
 int main() {
-    pthread_t tQueue, tReadFile;
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;    
     SDL_Event event;    
@@ -47,6 +40,7 @@ int main() {
     if (!initializeSDL(&window, &renderer)) {
         return -1;
     }
+
     SDL_mutex* mutex = SDL_CreateMutex();
     SharedData sharedData = { 0, 0 }; // 0 => all red
     
@@ -56,27 +50,29 @@ int main() {
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
     drawRoadsAndLane(renderer, font);
-    // drawLightForB(renderer, false);
     SDL_RenderPresent(renderer);
 
-    // we need to create seprate long running thread for the queue processing and light
-    // pthread_create(&tLight, NULL, refreshLight, &sharedData);
-    pthread_create(&tQueue, NULL, chequeQueue, &sharedData);
-    pthread_create(&tReadFile, NULL, readAndParseFile, NULL);
-    // readAndParseFile();
+    // Create Windows threads
+    HANDLE hQueueThread = CreateThread(NULL, 0, chequeQueue, &sharedData, 0, NULL);
+    HANDLE hReadThread = CreateThread(NULL, 0, readAndParseFile, NULL, 0, NULL);
 
     // Continue the UI thread
     bool running = true;
     while (running) {
-        // update light
         refreshLight(renderer, &sharedData);
         while (SDL_PollEvent(&event))
             if (event.type == SDL_QUIT) running = false;
+        Sleep(10); // small delay to avoid CPU overuse
     }
+
     SDL_DestroyMutex(mutex);
     if (renderer) SDL_DestroyRenderer(renderer);
     if (window) SDL_DestroyWindow(window);
-    // pthread_kil
+
+    // Wait for threads to finish (optional)
+    WaitForSingleObject(hQueueThread, 0);
+    WaitForSingleObject(hReadThread, 0);
+
     SDL_Quit();
     return 0;
 }
@@ -86,12 +82,10 @@ bool initializeSDL(SDL_Window **window, SDL_Renderer **renderer) {
         SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
         return false;
     }
-    // font init
     if (TTF_Init() < 0) {
         SDL_Log("SDL_ttf could not initialize! TTF_Error: %s\n", TTF_GetError());
         return false;
     }
-
 
     *window = SDL_CreateWindow("Junction Diagram",
                                SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
@@ -104,7 +98,6 @@ bool initializeSDL(SDL_Window **window, SDL_Renderer **renderer) {
     }
 
     *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
-    // if you have high resolution monitor 2K or 4K then scale
     SDL_RenderSetScale(*renderer, SCALE, SCALE);
 
     if (!*renderer) {
@@ -118,164 +111,120 @@ bool initializeSDL(SDL_Window **window, SDL_Renderer **renderer) {
     return true;
 }
 
-
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+// --- Thread functions ---
+DWORD WINAPI chequeQueue(LPVOID arg){
+    SharedData* sharedData = (SharedData*)arg;
+    while (1) {
+        sharedData->nextLight = 0;
+        Sleep(5000); // Windows Sleep in ms
+        sharedData->nextLight = 2;
+        Sleep(5000);
+    }
+    return 0;
 }
 
+DWORD WINAPI readAndParseFile(LPVOID arg) {
+    while(1){ 
+        FILE* file = fopen(VEHICLE_FILE, "r");
+        if (!file) {
+            perror("Error opening file");
+            Sleep(2000);
+            continue;
+        }
+
+        char line[MAX_LINE_LENGTH];
+        while (fgets(line, sizeof(line), file)) {
+            line[strcspn(line, "\n")] = 0;
+            char* vehicleNumber = strtok(line, ":");
+            char* road = strtok(NULL, ":");
+
+            if (vehicleNumber && road)  
+                printf("Vehicle: %s, Road: %s\n", vehicleNumber, road);
+            else 
+                printf("Invalid format: %s\n", line);
+        }
+        fclose(file);
+        Sleep(2000);
+    }
+    return 0;
+}
+
+// --- Other functions ---
+void swap(int *a, int *b) { int temp = *a; *a = *b; *b = temp; }
 
 void drawArrwow(SDL_Renderer* renderer, int x1, int y1, int x2, int y2, int x3, int y3) {
-    // Sort vertices by ascending Y (bubble sort approach)
     if (y1 > y2) { swap(&y1, &y2); swap(&x1, &x2); }
     if (y1 > y3) { swap(&y1, &y3); swap(&x1, &x3); }
     if (y2 > y3) { swap(&y2, &y3); swap(&x2, &x3); }
 
-    // Compute slopes
     float dx1 = (y2 - y1) ? (float)(x2 - x1) / (y2 - y1) : 0;
     float dx2 = (y3 - y1) ? (float)(x3 - x1) / (y3 - y1) : 0;
     float dx3 = (y3 - y2) ? (float)(x3 - x2) / (y3 - y2) : 0;
 
     float sx1 = x1, sx2 = x1;
 
-    // Fill first part (top to middle)
     for (int y = y1; y < y2; y++) {
         SDL_RenderDrawLine(renderer, (int)sx1, y, (int)sx2, y);
-        sx1 += dx1;
-        sx2 += dx2;
+        sx1 += dx1; sx2 += dx2;
     }
 
     sx1 = x2;
-
-    // Fill second part (middle to bottom)
     for (int y = y2; y <= y3; y++) {
         SDL_RenderDrawLine(renderer, (int)sx1, y, (int)sx2, y);
-        sx1 += dx3;
-        sx2 += dx2;
+        sx1 += dx3; sx2 += dx2;
     }
 }
 
-
 void drawLightForB(SDL_Renderer* renderer, bool isRed){
-    // draw light box
     SDL_SetRenderDrawColor(renderer, 150, 150, 150, 255);
     SDL_Rect lightBox = {400, 300, 50, 30};
     SDL_RenderFillRect(renderer, &lightBox);
-    // draw light
-    if(isRed) SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // red
-    else SDL_SetRenderDrawColor(renderer, 11, 156, 50, 255);    // green
+    if(isRed) SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    else SDL_SetRenderDrawColor(renderer, 11, 156, 50, 255);
     SDL_Rect straight_Light = {405, 305, 20, 20};
     SDL_RenderFillRect(renderer, &straight_Light);
     drawArrwow(renderer, 435,305, 435, 305+20, 435+10, 305+10);
 }
 
-
 void drawRoadsAndLane(SDL_Renderer *renderer, TTF_Font *font) {
     SDL_SetRenderDrawColor(renderer, 211,211,211,255);
-    // Vertical road
-    
     SDL_Rect verticalRoad = {WINDOW_WIDTH / 2 - ROAD_WIDTH / 2, 0, ROAD_WIDTH, WINDOW_HEIGHT};
     SDL_RenderFillRect(renderer, &verticalRoad);
-
-    // Horizontal road
     SDL_Rect horizontalRoad = {0, WINDOW_HEIGHT / 2 - ROAD_WIDTH / 2, WINDOW_WIDTH, ROAD_WIDTH};
     SDL_RenderFillRect(renderer, &horizontalRoad);
-    // draw horizontal lanes
+
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     for(int i=0; i<=3; i++){
-        // Horizontal lanes
-        SDL_RenderDrawLine(renderer, 
-            0, WINDOW_HEIGHT/2 - ROAD_WIDTH/2 + LANE_WIDTH*i,  // x1,y1
-            WINDOW_WIDTH/2 - ROAD_WIDTH/2, WINDOW_HEIGHT/2 - ROAD_WIDTH/2 + LANE_WIDTH*i // x2, y2
-        );
-        SDL_RenderDrawLine(renderer, 
-            800, WINDOW_HEIGHT/2 - ROAD_WIDTH/2 + LANE_WIDTH*i,
-            WINDOW_WIDTH/2 + ROAD_WIDTH/2, WINDOW_HEIGHT/2 - ROAD_WIDTH/2 + LANE_WIDTH*i
-        );
-        // Vertical lanes
-        SDL_RenderDrawLine(renderer,
-            WINDOW_WIDTH/2 - ROAD_WIDTH/2 + LANE_WIDTH*i, 0,
-            WINDOW_WIDTH/2 - ROAD_WIDTH/2 + LANE_WIDTH*i, WINDOW_HEIGHT/2 - ROAD_WIDTH/2
-        );
-        SDL_RenderDrawLine(renderer,
-            WINDOW_WIDTH/2 - ROAD_WIDTH/2 + LANE_WIDTH*i, 800,
-            WINDOW_WIDTH/2 - ROAD_WIDTH/2 + LANE_WIDTH*i, WINDOW_HEIGHT/2 + ROAD_WIDTH/2
-        );
+        SDL_RenderDrawLine(renderer, 0, WINDOW_HEIGHT/2 - ROAD_WIDTH/2 + LANE_WIDTH*i, WINDOW_WIDTH/2 - ROAD_WIDTH/2, WINDOW_HEIGHT/2 - ROAD_WIDTH/2 + LANE_WIDTH*i);
+        SDL_RenderDrawLine(renderer, 800, WINDOW_HEIGHT/2 - ROAD_WIDTH/2 + LANE_WIDTH*i, WINDOW_WIDTH/2 + ROAD_WIDTH/2, WINDOW_HEIGHT/2 - ROAD_WIDTH/2 + LANE_WIDTH*i);
+        SDL_RenderDrawLine(renderer, WINDOW_WIDTH/2 - ROAD_WIDTH/2 + LANE_WIDTH*i, 0, WINDOW_WIDTH/2 - ROAD_WIDTH/2 + LANE_WIDTH*i, WINDOW_HEIGHT/2 - ROAD_WIDTH/2);
+        SDL_RenderDrawLine(renderer, WINDOW_WIDTH/2 - ROAD_WIDTH/2 + LANE_WIDTH*i, 800, WINDOW_WIDTH/2 - ROAD_WIDTH/2 + LANE_WIDTH*i, WINDOW_HEIGHT/2 + ROAD_WIDTH/2);
     }
+
     displayText(renderer, font, "A",400, 10);
     displayText(renderer, font, "B",400,770);
     displayText(renderer, font, "D",10,400);
     displayText(renderer, font, "C",770,400);
-    
 }
 
-
 void displayText(SDL_Renderer *renderer, TTF_Font *font, char *text, int x, int y){
-    // display necessary text
-    SDL_Color textColor = {0, 0, 0, 255}; // black color
+    SDL_Color textColor = {0, 0, 0, 255};
     SDL_Surface *textSurface = TTF_RenderText_Solid(font, text, textColor);
     SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, textSurface);
     SDL_FreeSurface(textSurface);
-    SDL_Rect textRect = {x,y,0,0 };
+    SDL_Rect textRect = {x,y,0,0};
     SDL_QueryTexture(texture, NULL, NULL, &textRect.w, &textRect.h);
-    SDL_Log("DIM of SDL_Rect %d %d %d %d", textRect.x, textRect.y, textRect.h, textRect.w);
-    // SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    // SDL_Log("TTF_Error: %s\n", TTF_GetError());
     SDL_RenderCopy(renderer, texture, NULL, &textRect);
-    // SDL_Log("TTF_Error: %s\n", TTF_GetError());
 }
-
 
 void refreshLight(SDL_Renderer *renderer, SharedData* sharedData){
-    if(sharedData->nextLight == sharedData->currentLight) return; // early return
+    if(sharedData->nextLight == sharedData->currentLight) return;
 
-    if(sharedData->nextLight == 0){ // trun off all lights
-        drawLightForB(renderer, false);
-    }
+    if(sharedData->nextLight == 0){ drawLightForB(renderer, false); }
     if(sharedData->nextLight == 2) drawLightForB(renderer, true);
     else drawLightForB(renderer, false);
+
     SDL_RenderPresent(renderer);
-    printf("Light of queue updated from %d to %d\n", sharedData->currentLight,  sharedData->nextLight);
-    // update the light
+    printf("Light updated from %d to %d\n", sharedData->currentLight, sharedData->nextLight);
     sharedData->currentLight = sharedData->nextLight;
-    fflush(stdout);
-}
-
-
-void* chequeQueue(void* arg){
-    SharedData* sharedData = (SharedData*)arg;
-    int i = 1;
-    while (1) {
-        sharedData->nextLight = 0;
-        sleep(5);
-        sharedData->nextLight = 2;
-        sleep(5);
-    }
-}
-
-// you may need to pass the queue on this function for sharing the data
-void* readAndParseFile(void* arg) {
-    while(1){ 
-        FILE* file = fopen(VEHICLE_FILE, "r");
-        if (!file) {
-            perror("Error opening file");
-            continue;
-        }
-
-        char line[MAX_LINE_LENGTH];
-        while (fgets(line, sizeof(line), file)) {
-            // Remove newline if present
-            line[strcspn(line, "\n")] = 0;
-
-            // Split using ':'
-            char* vehicleNumber = strtok(line, ":");
-            char* road = strtok(NULL, ":"); // read next item resulted from split
-
-            if (vehicleNumber && road)  printf("Vehicle: %s, Raod: %s\n", vehicleNumber, road);
-            else printf("Invalid format: %s\n", line);
-        }
-        fclose(file);
-        sleep(2); // manage this time
-    }
 }
