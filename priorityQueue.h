611bfcc8b5dced966bf92d134111d1900bcb5d48
sudha-258 <karkisudha21@gmail.com>
2025-12-23 21:20:36 +0545

    priority queue header minor changes

--- priorityQueue.h
+++ priorityQueue.h
@@ -1,19 +1,37 @@
 #ifndef PRIORITY_QUEUE_H
 #define PRIORITY_QUEUE_H
 
-typedef struct {
-    char name[4];   // AL2, BL2 etc
-    int priority;
-} Lane;
+#include <stdbool.h>
 
+#define MAX_PQ_SIZE 10
+
+// Road information for priority queue
 typedef struct {
-    Lane data[10];
+    int roadIndex;     // 0=A, 1=B, 2=C, 3=D
+    int vehicleCount;  // Number of vehicles waiting
+    int priority;      // Lower value = higher priority
+    bool isPriorityLane;  // Is this the priority lane (AL2)?
+} RoadData;
+
+// Priority Queue structure (Min Heap)
+typedef struct {
+    RoadData data[MAX_PQ_SIZE];
     int size;
 } PriorityQueue;
 
-void initPQ(PriorityQueue *pq);
-void insertLane(PriorityQueue *pq, Lane l);
-Lane extractHighestPriority(PriorityQueue *pq);
-void updatePriority(PriorityQueue *pq, char *name, int p);
+// Function prototypes
+void initPriorityQueue(PriorityQueue* pq);
+bool isPQEmpty(PriorityQueue* pq);
+bool isPQFull(PriorityQueue* pq);
+void pqInsert(PriorityQueue* pq, RoadData rd);
+RoadData pqExtractMin(PriorityQueue* pq);
+RoadData pqPeek(PriorityQueue* pq);
+void pqUpdatePriority(PriorityQueue* pq, int roadIndex, int newPriority);
+void displayPriorityQueue(PriorityQueue* pq);
 
-#endif
+// Helper functions
+void heapifyUp(PriorityQueue* pq, int index);
+void heapifyDown(PriorityQueue* pq, int index);
+void swap(RoadData* a, RoadData* b);
+
+#endif // PRIORITY_QUEUE_H
